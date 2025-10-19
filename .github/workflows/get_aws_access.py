1d1ac02e5abb8eb3e57dbf896d0defda46115148
lucasrocha2704 <lucas.rocha@sptech.school>
2025-10-19 14:49:12 -0300

    fix: adição de logging

--- .github/workflows/get_aws_access.py
+++ .github/workflows/get_aws_access.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import sync_playwright
-import time
+import time, os, logging
+
+logging.basicConfig(level=logging.INFO)
 
 class AWS:
     def __init__(self, email, password, headless=True):
@@ -8,12 +10,12 @@ class AWS:
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
 
-        print("[+] Navegador iniciado")
+        logging.info("[+] Navegador iniciado")
         self._login(email, password)
 
     def _login(self, email, password):
         """Realiza login no portal AWS Academy."""
-        print("[*] Fazendo login...")
+        logging.info("[*] Fazendo login...")
         self.page.goto("https://www.awsacademy.com/vforcesite/LMS_Login")
 
         self.page.click('body > div.splash-body > a:nth-child(1) > button')
@@ -23,32 +25,35 @@ class AWS:
 
         self.page.click('#login_form > div.ic-Login__actions > div.ic-Form-control.ic-Form-control--login > input')
 
-        print("[+] Login concluído com sucesso")
+        logging.info("[+] Login concluído com sucesso")
 
 
     def configure_aws(self, conta):
         """Configura a conta da AWS para consulta"""
-        print("[+] Entrando na AWS")
+        logging.info("[+] Entrando na AWS")
         self.page.goto(f"https://awsacademy.instructure.com/courses/{conta}/modules/items/12498015")
         self.page.wait_for_load_state("domcontentloaded", timeout=0)
 
-        print('[*] Verificando status da conta')
+        logging.info('[*] Verificando status da conta')
         frame_locator = self.page.frame_locator('iframe.tool_launch')
         try:
             locator = frame_locator.locator('#vmstatus')
             locator.wait_for(state='attached', timeout=30000)
             classe = locator.get_attribute('class')
         except Exception as e:
+            screenshot_dir = "screenshots"
+            os.makedirs(screenshot_dir, exist_ok=True)
             timestamp = int(time.time())
-            self.page.screenshot(path=f'screenshot_{timestamp}.png')
-            print(f"Erro encontrado: {e}")
+            path = os.path.join(screenshot_dir, f"screenshot_{timestamp}.png")
+            self.page.screenshot(path=path)
+            logging.info(f"Screenshot salvo em: {path}")
 
         if 'led-green' not in classe:
-            print('[*] Inicia a conta da AWS')
+            logging.info('[*] Inicia a conta da AWS')
             frame_locator.locator('#launchclabsbtn').click()
             frame_locator.locator('#vmstatus.led-green').wait_for(timeout=0)
 
-        print("[*] Coletando informações da AWS")
+        logging.info("[*] Coletando informações da AWS")
         # mostrar informações aws
         frame_locator.locator('#detailbtn2').click()
         frame_locator.locator('#clikeyboxbtn').click()
@@ -66,7 +71,7 @@ class AWS:
         aws_access_key_id = info[1].split('=')[1]
         aws_secret_access_key = info[2].split('=')[1]
         aws_session_token = info[3].split('=', 1)[1]
-        print('[+] Dados coletados')
+        logging.info('[+] Dados coletados')
         return aws_access_key_id, aws_secret_access_key, aws_session_token
 
 def set_github_env(aws_access_key_id, aws_secret_access_key, aws_session_token):
